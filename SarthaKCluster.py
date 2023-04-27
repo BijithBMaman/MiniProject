d7d1f6965234782bbe7f2ec10c581a51af94a815
Bijith <bijithbmaman@gmail.com>
2023-04-27 20:45:46 +0530

    Small change

--- SarthaKCluster.py
+++ SarthaKCluster.py
@@ -10,7 +10,7 @@ data = pd.read_csv(r'C:\Users\Public\My Project\Miniproject\MiniProject\filtered
 
 # Define the categorical and numerical columns
 cat_cols = ['Source', 'Destination', 'Protocol']
-num_cols = ['Length','Time']
+num_cols = ['Time', 'Length']
 
 # Define the column transformer
 ct = ColumnTransformer([
@@ -22,26 +22,32 @@ ct = ColumnTransformer([
 X = ct.fit_transform(data)
 
 # Define the k-means clustering algorithm
-kmeans = KMeans(n_clusters=2, random_state=42)
-
+kmeans = KMeans(n_clusters=2, random_state=10)
 
 # Fit the k-means clustering algorithm to the data
-kmeans.fit(X)
+kmeans.fit(X) # only use the first column (Time) for clustering
 
 # Print the cluster labels for each data point
 print(kmeans.labels_)
 
-labels= kmeans.labels_
+labels = kmeans.labels_
 data['Label'] = labels
 
 # Save the DataFrame with the labels to a new CSV file
 data.to_csv('labeled_data2.csv', index=False)
 
-# Plot scatter plot
+# Plot scatter plot of Soure and time of labeled data
+plt.scatter(data['Source'], data['Time'])
+plt.xlabel('Source')
+plt.ylabel('Time')
+plt.title('Scatter plot of Soure and time of labeled data')
+plt.show()
+
+# Plot scatter plot of Soure and label of labeled data
 plt.scatter(data['Source'], data['Label'])
 plt.xlabel('Source')
 plt.ylabel('Label')
-plt.title('Scatter plot of labeled data')
+plt.title('Scatter plot of Soure and Label of labeled data')
 plt.show()
 
 # Plot graph
@@ -53,13 +59,13 @@ plt.title('Graph of labeled data')
 plt.show()
 
 
-# Get the cluster centroids
-centroids = kmeans.cluster_centers_
+# # Get the cluster centroids
+# centroids = kmeans.cluster_centers_
 
-# Create a DataFrame of the cluster centroids with the correct column names
-cat_col_names = ct.named_transformers_['encoder'].get_feature_names(cat_cols)
-col_names = list(cat_col_names) + num_cols
-centroid_df = pd.DataFrame(centroids, columns=col_names)
+# # Create a DataFrame of the cluster centroids with the correct column names
+# cat_col_names = ct.named_transformers_['encoder'].get_feature_names(cat_cols)
+# col_names = list(cat_col_names) + num_cols
+# centroid_df = pd.DataFrame(centroids, columns=col_names)
 
 # # Plot a heatmap of the cluster centroids
 # sns.heatmap(centroid_df, cmap="YlGnBu")
